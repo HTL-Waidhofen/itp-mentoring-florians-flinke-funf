35ed86dcf0baa80922e4f9af2b78dd4a6f3b1b8b
Fabian Neuheimer <fabian.neuheimer@htlwy.at>
2023-11-16 09:50:08 +0100

    Basic structure

---
+++ Mentoring-App/Mentoring-App/Pages/Mentor.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mentoring_App.Pages
+{
+    /// <summary>
+    /// Interaktionslogik für Mentor.xaml
+    /// </summary>
+    public partial class Mentor : Page
+    {
+        public Mentor()
+        {
+            InitializeComponent();
+        }
+    }
+}
