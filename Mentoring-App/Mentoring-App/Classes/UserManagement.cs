f4d8090cd1a6e8cfbe5339bc2eb934d91cc238df
Maierhofer Kilian <kilian.maierhofer@htlwy.at>
2023-12-07 09:28:23 +0100

    added functions

--- Mentoring-App/Mentoring-App/Classes/UserManagement.cs
+++ Mentoring-App/Mentoring-App/Classes/UserManagement.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Mentoring_App
 {
@@ -281,6 +282,48 @@ namespace Mentoring_App
         private static string loadConnectionString()
         {
             return "DataSource=MentoringDB.db;Version=3;";
+        }
+        public static bool confirmLogin(string usermail, string password)
+        {
+            students = LoadStudentsFromDB();
+            mentors = LoadMentorsFromDB();
+
+            foreach (var s in students)
+            {
+                if (s.Email == usermail && s.Password == password)
+                {
+                    return true;
+                }
+            }
+            foreach (var m in mentors)
+            {
+                if (m.Email == usermail && m.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool IsEmailValid(string email)
+        {
+            Regex regex = new Regex(@"^(\w{1,50}.?\w{1,50}@htlwy.at)$");
+            return (regex.IsMatch(email));
+        }
+        public static string GetUserStatus(string email)
+        {
+            students = LoadStudentsFromDB();
+            mentors = LoadMentorsFromDB();
+            foreach (Mentor m in mentors)
+            {
+                if (m.Email == email)
+                    return "mentor";
+            }
+            foreach (Student s in students)
+            {
+                if (s.Email == email)
+                    return "student";
+            }
+            return "not available";
         }
     }
 }
